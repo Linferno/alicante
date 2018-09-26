b00ef9318eb8cf36752f261631af44484f69ec29
Masa <mkawaba2@u.rochester.edu>
2018-09-26 16:14:37 -0400

    content change in xinyi

--- xchen.php
+++ xchen.php
@@ -2,66 +2,49 @@
 
 	<body>
 
-
 		<header>
 			<h1>Xinyi Chen</h1>
-	
 		</header>
 
 
 <?php include "inc/nav.inc"; ?> 
 			
-			<article>
-
+		<article>
 		
-			<h2>About Xinyi</h2>
+			<h2>Story of Xinyi</h2>
+
 	        <figure> 
 				<img src="images/tianjin.png" alt="tianjin">
 			</figure>
 
 			<p>
-				I am a digital media study undergraduate student of University of Rochester. I am also trying to major or minor in Spanish. I am that kind of person who is not really specialized or skilled in any specific field but always willing to try something new. I am not sure if it is a good thing or a bad thing to be exploring around different possibilities and not commit myself in one particular, but that is what I am right now, at least in this stage of my life. My computer science skill includes basic HTML, CSS, python. My other skills include cooking weird food, drawing grotesque, speaking a little chin-span-glish, and chin-span-urk.
+				Xinyi is a digital media study undergraduate student of University of Rochester. She is also trying to major or minor in Spanish. She is that kind of person who is not really specialized or skilled in any specific field but always willing to try something new. She is not sure if it is a good thing or a bad thing to be exploring around different possibilities and not commit myself in one particular, but that is what she is right now, at least in this stage of her life. Her computer science skill includes basic HTML, CSS, python. Her other skills include cooking weird food, drawing grotesque, speaking a little chin-span-glish, and chin-span-urk.
 			</p>
 
 
 			<h2>personal</h2>
-	        <figure> 
-				<img src="images/bangkok.png" alt="bangkok">
-				<figcaption> In Bangkok, Thailand</figcaption>
-			</figure>
-
-			
 
 			<p>
-				I have been living in Beijing, the capital of China for the past 15 years and it is my first years living in U.S. My first language is Chinese, but the first language I learnt is Cantonese, even though I already forget everything. When I was small, I was sent to the south and then set back to Beijing. Beijing is a gigantic city full of spectaculars. You can see the relic of the ancient empire standing in between metropolitan skyscrapers. You can taste authentic local food from all over the China, since 70% of its population are originally none-Beijingers. You can compete with the “people mountain people sea” in every corner within the five transportation circles. Growing up in Beijing, I was deeply influenced by its personality—unsettled, always craving for change and refinement, open to new.
+				She has been living in Beijing, the capital of China for the past 15 years and it is her first years living in U.S. Her first language is Chinese, but the first language I learnt is Cantonese, even though she already forget everything. When she was small, she was sent to the south and then set back to Beijing. Beijing is a gigantic city full of spectaculars. You can see the relic of the ancient empire standing in between metropolitan skyscrapers. You can taste authentic local food from all over the China, since 70% of its population are originally none-Beijingers. You can compete with the “people mountain people sea” in every corner within the five transportation circles. Growing up in Beijing, she was deeply influenced by its personality—unsettled, always craving for change and refinement, open to new.
 
 			</p>
 
 
             <h2>Jobs</h2>
-	        <figure> 
-				<img src="images/beijing.png" alt="beijing">
-				<figcaption> In Beijing, China</figcaption>
-			</figure>
 
-		
-			<p>Here in University of Rochester, there’s not as many things to do as in Beijing. Rochester is a quite, small, peaceful, nice little town for me, even though my former roommate from upstate New York told me it is actually a “city”. I did manage to some jobs to do by fortune. I am working as a teaching assistant in CSC170, and a note-taker in AME294 this semester. I never had a job here before. In Beijing, I used to teach and take care kids from high school and primary school. I never had a job which is not related to education, though I really would love to be a coffee shop owner deeply inside my heart. I believe in a way every job merits equally, regardless how many people you serve, what you do, how many dollars float. Because what really matters is that you gain a sense of achievement and respect yourself.
+			<p>
+				Here in University of Rochester, there’s not as many things to do as in Beijing. Rochester is a quite, small, peaceful, nice little town for me, even though her former roommate from upstate New York told her it is actually a “city”. She did manage to some jobs to do by fortune. She is working as a teaching assistant in CSC170, and a note-taker in AME294 this semester. She never had a job here before. In Beijing, she used to teach and take care kids from high school and primary school. She never had a job which is not related to education, though she really would love to be a coffee shop owner deeply inside my heart. She believes in a way every job merits equally, regardless how many people you serve, what you do, how many dollars float. Because what really matters is that she gains a sense of achievement and respect yourself.
 
 			</p>
 			
 			<h2>Courses</h2>
-	        <figure> 
-				<img src="images/shanghai.png" alt="shanghai">
-				<figcaption>In Shanghai, China</figcaption>
-			</figure>
-
 
 			<p>
-				I have taken courses from various subjects. I have taken all the four digital media study core courses. I also took writing and middle east dance the first semester of my freshman year. I took the two elementary Spanish courses and right now I’m taking the intermediate two Spanish. I also took elementary Turkish the second semester of my freshman year, but unfortunately the professor is leaving the university and all the Turkish language courses are canceled. This semester I am also taking resistant literature of the middle east, advanced front-end web developing, revolution in sound, and introduction to book publishing. 
+				She has taken courses from various subjects. She has taken all the four digital media study core courses. She also took writing and middle east dance the first semester of my freshman year. She took the two elementary Spanish courses and right now she is taking the intermediate two Spanish. She also took elementary Turkish the second semester of my freshman year, but unfortunately the professor is leaving the university and all the Turkish language courses are canceled. This semester she is also taking resistant literature of the middle east, advanced front-end web developing, revolution in sound, and introduction to book publishing. 
 
 			</p>
 			
-			</article>
+		</article>
 
 
 	<a href="urcsc174.org/assignment01/xchen/index.php">Original Website</a>
