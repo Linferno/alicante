4dab27ec04306c25bc46c9397173bd7e3f648bb3
Masa <mkawaba2@u.rochester.edu>
2018-09-26 16:22:29 -0400

    Added h2 edited content

--- rxiao.php
+++ rxiao.php
@@ -7,41 +7,45 @@
 
 <?php include "inc/nav.inc";?>
 
-<figure>
-	<img src="images/jerry.png" alt="jerry photo">
-	<figcaption>A photo of young Renjie</figcaption>
-</figure>
+	<article>
 
-<p>
-	I am an international student currently major in Business Analytics and minor in Computer Science at the University of Rochester. Before that I completed my high school at Walsingham Academy in a little town named Williamsburg. And before that, I have lived and studied in Shanghai, China for 16 years. 
+		<figure>
+			<img src="images/jerry.png" alt="jerry photo">
+			<figcaption>A photo of Renjie</figcaption>
+		</figure>
 
+		<p>
+			Renjie is an international student currently major in Business Analytics and minor in Computer Science at the University of Rochester. Before that he completed my high school at Walsingham Academy in a little town named Williamsburg. He has lived and studied in Shanghai, China for 16 years. 
+		</p>
 
-</p>
+	<h2>Personal:</h2>
 
-<p>
-	In 2012, I came to the states at 16 years old as an exchange student in Williamsburg. Living in Shanghai for my entire live, I have never left my family for longer than a week. So, coming to a totally different country and study excites me and frighten me at the same time. The culture shock really gets to me the first few weeks, but after that my study live here just gets better and better with the help of friendly classmates and my host parents. 
+		<p>
+			In 2012, he came to the states at 16 years old as an exchange student in Williamsburg. Living in Shanghai for his entire live, he has never left my family for longer than a week. So, coming to a totally different country and study excites him and frighten him at the same time. The culture shock really gets to him the first few weeks, but after that his study live here just gets better and better with the help of friendly classmates and his host parents. 
+		</p>
 
-</p>
+		<p>
+			For the three years in Walsingham Academy, one thing that help him the most are being part of the tennis team. He did not know how to play when he first started, and his English wasn’t good enough to let him communicate with others. That depressed him quite a bit because he can only watch my teammates play on matches while he sit on the bench. Weeks later, with the help of his coach, he finally get the hang of it and his language and social skills improved with his tennis skills.  With the confidence he gained from success in tennis, graduated with honor and got accepted into University of Rochester.
 
-<p>
-	For the three years in Walsingham Academy, one thing that help me the most are being part of the tennis team. I did not know how to play when I first started, and my English wasn’t good enough to let me communicate with others. That depressed my quite a bit because I can only watch my teammates play on matches while I sit on the bench. Weeks later, with the help of my coach, I finally get the hang of it and my language and social skills improved with my tennis skills.  With the confidence I gained from success in tennis, I graduated with honor and got accepted into University of Rochester.
+		</p>
 
+	<h2>Jobs:</h2>
 
-</p>
+		<p>
+			For the summer of my sophomore year, I applied for an internship at an asset management company in Shanghai. As I started as an equity research intern in this company, I learned multiple ways to analysis a company and its stock price to see if they are worthy to invest. On the other hand, I have gained interest on business analyst from many internal meetings, that is to use big data and programming to predict future movement of stocks. After I did more research on business analyst, and taking some classes in it, I decided to switch my major into business analyst. 
+		</p>
 
+	<h2>Academics:</h2>
 
-<p>
-	Besides the normal programming classes, I have also found interest in website design after taking CSC170, which is an awesome course to introduce the world of web design.  During this class, I have learned so much about CSS,HTML and JS. I have also learned how to put these components together and make them a workable website. After that, I decided to add computer science as my minor and continue to build up more knowledge about web design by taking more advanced classes like csc174. 
+		<p>
+			In 2015, I came to Rochester to pursue my bachelor’s degree. Started off with talking econ108 with Pr. Rizzo, I soon gain great interest in the field of economics because he made the class interesting and easy to understand. After taking more courses with him, I declared my major as Financial Economics at the end of the sophomore year. At that time, I think economics will be my greatest passion in the future. 
+		</p>
 
-</p>
+		<p>
+			Besides the normal programming classes, I have also found interest in website design after taking CSC170, which is an awesome course to introduce the world of web design.  During this class, I have learned so much about CSS,HTML and JS. I have also learned how to put these components together and make them a workable website. After that, I decided to add computer science as my minor and continue to build up more knowledge about web design by taking more advanced classes like csc174. 
+		</p>
 
-<p>
-In 2015, I came to Rochester to pursue my bachelor’s degree. Started off with talking econ108 with Pr. Rizzo, I soon gain great interest in the field of economics because he made the class interesting and easy to understand. After taking more courses with him, I declared my major as Financial Economics at the end of the sophomore year. At that time, I think economics will be my greatest passion in the future. 
-</p>
-
-<p>
-	For the summer of my sophomore year, I applied for an internship at an asset management company in Shanghai. As I started as an equity research intern in this company, I learned multiple ways to analysis a company and its stock price to see if they are worthy to invest. On the other hand, I have gained interest on business analyst from many internal meetings, that is to use big data and programming to predict future movement of stocks. After I did more research on business analyst, and taking some classes in it, I decided to switch my major into business analyst. 
-</p>
+	</article>
 <a href="urcsc174.org/assignment01/rxiao2/index.php">Original Website</a>
 <?php include "inc/scripts.inc"; ?>
 
