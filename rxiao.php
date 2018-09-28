51f5df5fd5a81f9ba303069ee5e67dad1afe381c
Masa <mkawaba2@u.rochester.edu>
2018-09-28 09:35:32 -0400

    content edit
    Corrected grammatical error

--- rxiao.php
+++ rxiao.php
@@ -10,38 +10,37 @@
 
 		<figure>
 			<img src="images/jerry.png" alt="jerry photo">
-			<figcaption>Photo of Renjie</figcaption>
 		</figure>
 
 		<p>
-			Renjie is an international student currently major in Business Analytics and minor in Computer Science at the University of Rochester. Before that he completed my high school at Walsingham Academy in a little town named Williamsburg. He has lived and studied in Shanghai, China for 16 years. 
+			Renjie is an international student currently major in Business Analytics and minor in Computer Science at the University of Rochester. Before that, he completed his high school at Walsingham Academy in a little town named Williamsburg. He has lived and studied in Shanghai, China for 16 years. 
 		</p>
 
 	<h2>Personal:</h2>
 
 		<p>
-			In 2012, he came to the states at 16 years old as an exchange student in Williamsburg. Living in Shanghai for his entire live, he has never left my family for longer than a week. So, coming to a totally different country and study excites him and frighten him at the same time. The culture shock really gets to him the first few weeks, but after that his study live here just gets better and better with the help of friendly classmates and his host parents. 
+			In 2012, he came to the states at 16 years old as an exchange student in Williamsburg. Living in Shanghai for his entire live, he has never left his family for longer than a week. So, coming to a totally different country and study excited and frighten him at the same time. The culture shock really got to him the first few weeks, but gradually he adjusted into the American culture with the help of friendly classmates and his host parents.
 		</p>
 
 		<p>
-			For the three years in Walsingham Academy, one thing that help him the most are being part of the tennis team. He did not know how to play when he first started, and his English wasn’t good enough to let him communicate with others. That depressed him quite a bit because he can only watch my teammates play on matches while he sit on the bench. Weeks later, with the help of his coach, he finally get the hang of it and his language and social skills improved with his tennis skills.  With the confidence he gained from success in tennis, graduated with honor and got accepted into University of Rochester.
+			For the three years in Walsingham Academy, one thing that help him the most was being part of the tennis team. He did not know how to play when he first started, and his English wasn’t good enough to let him communicate with others. That depressed him quite a bit because he can only watch my teammates play on matches while he sat on the bench. Weeks later, with the help of his coach, he finally got the hang of it and his social skills improved with his tennis skills.  With the confidence he gained from success in tennis, graduated with honor and got accepted into University of Rochester.
 
 		</p>
 
 	<h2>Jobs:</h2>
 
 		<p>
-			For the summer of my sophomore year, he applied for an internship at an asset management company in Shanghai. As he started as an equity research intern in this company, he learned multiple ways to analysis a company and its stock price to see if they are worthy to invest. On the other hand, he has gained interest on business analyst from many internal meetings, that is to use big data and programming to predict future movement of stocks. After he did more research on business analyst, and taking some classes in it, he decided to switch my major into business analyst. 
+			For the summer of his sophomore year, he applied for an internship at an asset management company in Shanghai. As he started as an equity research intern in this company, he learned multiple ways to analyze a company and its stock price to see if they are worthy to invest. On the other hand, he has gained interest on business analyst from many internal meetings, which is to use big data and programming to predict future movement of stocks. After he did more research on business analyst, and taking some classes in it, he decided to switch his major into business analyst.
 		</p>
 
 	<h2>Academics:</h2>
 
 		<p>
-			In 2015, he came to Rochester to pursue my bachelor’s degree. Started off with talking ECO 108 with Pr. Rizzo, he soon gain great interest in the field of economics because he made the class interesting and easy to understand. After taking more courses with him, he declared my major as Financial Economics at the end of the sophomore year. At that time, he thought economics will be his greatest passion in the future. 
+			In 2015, he came to Rochester to pursue his bachelor’s degree. Started off the college academics taking ECO 108 with Pr. Rizzo, Renjie soon gained great interest in the field of economics because he made the class interesting and easy to understand. After taking more courses with him, he declared his major as Financial Economics at the end of the sophomore year. At that time, he thought economics will be his greatest passion in the future.
 		</p>
 
 		<p>
-			Besides the normal programming classes, he has also found interest in website design after taking CSC170, which is an awesome course to introduce the world of web design.  During this class, he has learned so much about HTML, CSS and JS. He has also learned how to put these components together and make them a workable website. After that, he decided to add computer science as my minor and continue to build up more knowledge about web design by taking more advanced classes like csc174. 
+			Besides the normal programming classes, he has also found interest in website design after taking CSC170, which was an awesome course to introduce the world of web design.  During this class, he has learned so much about HTML, CSS and JS. He also learned how to put these components together and make them as a workable website. After that, he decided to add computer science as his minor and continue to build up more knowledge about web design by taking more advanced classes such as csc174.
 		</p>
 
 	</article>
--- xchen.php
+++ xchen.php
@@ -15,33 +15,31 @@
 
 	        <figure> 
 				<img src="images/tianjin.png" alt="tianjin">
-				<figcaption>Photo of Xinyi</figcaption>
 			</figure>
 
 			<p>
-				Xinyi is a digital media study undergraduate student of University of Rochester. She is also trying to major or minor in Spanish. She is that kind of person who is not really specialized or skilled in any specific field but always willing to try something new. She is not sure if it is a good thing or a bad thing to be exploring around different possibilities and not commit myself in one particular, but that is what she is right now, at least in this stage of her life. Her computer science skill includes basic HTML, CSS, python. Her other skills include cooking weird food, drawing grotesque, speaking a little chin-span-glish, and chin-span-urk.
+				Xinyi is a digital media study undergraduate student of University of Rochester. She is also trying to major or minor in Spanish. She is a person who is not really specialized or skilled in any specific field but always willing to try something new. Xinyi is not sure if it is a good idea exploring around different possibilities and not commit herself in one particular subject, but she understands this is what college is for. Her computer science skill includes basic HTML, CSS, python. Her hobbies include cooking weird food, drawing grotesque, speaking a little chin-span-glish, and chin-span-urk.
 			</p>
 
 
 			<h2>Personal:</h2>
 
 				<p>
-					She has been living in Beijing, the capital of China for the past 15 years and it is her first years living in U.S. Her first language is Chinese, but the first language I learnt is Cantonese, even though she already forget everything. When she was small, she was sent to the south and then set back to Beijing. Beijing is a gigantic city full of spectaculars. You can see the relic of the ancient empire standing in between metropolitan skyscrapers. You can taste authentic local food from all over the China, since 70% of its population are originally none-Beijingers. You can compete with the “people mountain people sea” in every corner within the five transportation circles. Growing up in Beijing, she was deeply influenced by its personality—unsettled, always craving for change and refinement, open to new.
-
+					Xinyi has been living in Beijing, the capital of China for the past 15 years and this is her first years living in U.S. Her primary language is Chinese, but the first language she learned was Cantonese, even though she already forget everything. Beijing is a gigantic city full of spectaculars. You can see the relic of the ancient empire standing in between metropolitan skyscrapers. You can taste authentic local food from all over the China, since 70% of the population is originally from the outside of Beijing. Growing up in Beijing, she was deeply influenced by its environment - unsettled, always craving for change and refinement, and open to new ideas.
 				</p>
 
 
             <h2>Jobs:</h2>
 
 				<p>
-					Here in University of Rochester, there’s not as many things to do as in Beijing. Rochester is a quite, small, peaceful, nice little town for me, even though her former roommate from upstate New York told her it is actually a “city”. She did manage to some jobs to do by fortune. She is working as a teaching assistant in CSC170, and a note-taker in AME294 this semester. She never had a job here before. In Beijing, she used to teach and take care kids from high school and primary school. She never had a job which is not related to education, though she really would love to be a coffee shop owner deeply inside my heart. She believes in a way every job merits equally, regardless how many people you serve, what you do, how many dollars float. Because what really matters is that she gains a sense of achievement and respect yourself.
+					Here in University of Rochester, there is not as many things to do as compared to Beijing. Rochester is a peaceful, nice small town for Xinyi, even though her former roommate from upstate New York told her it is actually a “city”. She did manage to some jobs to do by fortune. Currently, Xinyi is working as a teaching assistant in CSC170, and a note-taker in AME294 this semester. She never had a job here before in the U.S. In Beijing, she used to teach and took care kids from high school and primary school. She never had a job which is not related to education, though she really wants to become a coffee shop owner deeply inside her heart. She believes every job merits equally, regardless how many people you serve, what you do, how many dollars float. Because what really matters is that she gains a sense of achievement and respect herself.
 
 				</p>
 			
 			<h2>Academics:</h2>
 
 				<p>
-					She has taken courses from various subjects. She has taken all the four digital media study core courses. She also took writing and middle east dance the first semester of my freshman year. She took the two elementary Spanish courses and right now she is taking the intermediate two Spanish. She also took elementary Turkish the second semester of my freshman year, but unfortunately the professor is leaving the university and all the Turkish language courses are canceled. This semester she is also taking resistant literature of the middle east, advanced front-end web developing, revolution in sound, and introduction to book publishing. 
+					She has taken courses from various subjects. She has taken all of the four digital media study core courses. She also took writing and Middle East dance the first semester of her freshman year. She took two elementary Spanish courses and currently enrolled in intermediate Spanish class. She explored and took elementary Turkish her second semester in freshman year, however the professor is leaving the university and all of the Turkish language courses had to be cancelled. In this semester, she is taking resistant literature of the middle east, advanced front-end web developing, revolution in sound, and introduction to book publishing.
 
 				</p>
 			
