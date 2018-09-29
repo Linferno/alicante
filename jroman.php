21902a9885d80b1e6f564fe3c66dbe51c2d012ff
Masa <mkawaba2@u.rochester.edu>
2018-09-28 20:29:15 -0400

    content edit

--- jroman.php
+++ jroman.php
@@ -16,15 +16,18 @@
 		Jonathan is from Tenafly, NJ and was born September 6th, 1997. He currently attends to the University of Rochester with a major in Computer Science. His hobbies include playing video games and basketball, both of which he participates in groups around campus for.
 	</p>
 
-	<h2>Academics:</h2>
-
-		<p>
-			As far as his academic career used to be, he was very into biology in high school. He would attend science competitions and fairs to present research he had begun through a special science research program in my school (which he credits his acceptance to the University of Rochester to). Since he was still a teenager, he had not really explored many fields up to this point and although the idea of biology excited him, he does not believe he was truly passionate about it. He had always been into video games and technology since he was old enough to operate a screen, so it seemed logical that he would like computer science, but my school did not offer anything in the field, so I entered the University of Rochester with a major in biology. Once he arrived at college, he continued with my track for a year or two before he began taking classes in computer science and instantly fell in love with it. He did not realize how much he would truly enjoy the field but he is extremely happy he will end up being able to complete my major within the 4 years even with some extra classes from other fields under his belt.
-		</p>
+	<h2>Personal:</h2>
 
 		<p>
 			His hobbies slightly changed when he arrived to college. He, for the most part, stopped playing tennis and picked up basketball as his favorite sport. Since then he has tried to play nearly every day, and he is currently the captain of the men's club basketball team here at the University of Rochester. It's been a very different experience to transition from an individually dependent sport such as tennis to a fully organized team sport like basketball. It's been a learning experience and really has helped him grow as a person in regards to his ability to work well with other people / teammates. His interest in gaming remains mostly unchanged, and he is still keeping up to date with all the changing information in that industry.
 		</p>
+
+	<h2>Academics:</h2>
+
+		<p>
+			As far as his academic career used to be, he was very into biology in high school. He would attend science competitions and fairs to present research he had begun through a special science research program in his school (which he credits his acceptance to the University of Rochester to). Since he was still a teenager, he had not really explored many fields up to this point and although the idea of biology excited him, he does not believe he was truly passionate about it. He had always been into video games and technology since he was old enough to operate a screen, so it seemed logical that he would like computer science, but his school did not offer anything in the field, so he entered the University of Rochester with a major in biology. Once he arrived at college, he continued with my track for a year or two before he began taking classes in computer science and instantly fell in love with it. He did not realize how much he would truly enjoy the field but he is extremely happy he ended up being able to complete his major within the 4 years even with some extra classes from other fields under his belt.
+		</p>
+
 
 	<h2>Jobs:</h2>
 
